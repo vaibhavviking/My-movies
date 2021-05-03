e1df396a9fdf880827a693327d981de9fd5d54e8
ChakPC <ee190002048@iiti.ac.in>
2021-05-04 05:07:18 +0530

    Home Page

---
+++ views/js/movies.js
@@ -0,0 +1,227 @@
+var movies ={
+    "Adventure" : 
+    [
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000165384395-rhrjdn-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000165384395-rhrjdn-t500x500.jpg",
+            original_title: "3 Idiots",
+            vote_average: 9
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000185743981-tuesoj-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000185743981-tuesoj-t500x500.jpg",
+            original_title: "Iron Man",
+            vote_average: 9
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000158708482-k160g1-t500x500.jpg",
+            backdrop_path: null,
+            original_title: "Avengers Infinity War",
+            vote_average: 8.5
+        },
+        {
+            poster_path: null,
+            backdrop_path: "https://i1.sndcdn.com/artworks-000062423439-lf7ll2-t500x500.jpg",
+            original_title: "Bahubali: The Beginning",
+            vote_average: 8
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000028787381-1vad7y-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000028787381-1vad7y-t500x500.jpg",
+            original_title: "Super 30",
+            vote_average: 8.5
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000108468163-dp0b6y-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000108468163-dp0b6y-t500x500.jpg",
+            original_title: "Mirzapur",
+            vote_average: 7
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000064920701-xrez5z-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000064920701-xrez5z-t500x500.jpg",
+            original_title: "John Wick",
+            vote_average: 7.5
+        }    
+    ],
+    "Action":
+    [
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000165384395-rhrjdn-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000165384395-rhrjdn-t500x500.jpg",
+            original_title: "3 Idiots",
+            vote_average: 9
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000185743981-tuesoj-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000185743981-tuesoj-t500x500.jpg",
+            original_title: "Iron Man",
+            vote_average: 9
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000158708482-k160g1-t500x500.jpg",
+            backdrop_path: null,
+            original_title: "Avengers Infinity War",
+            vote_average: 8.5
+        },
+        {
+            poster_path: null,
+            backdrop_path: "https://i1.sndcdn.com/artworks-000062423439-lf7ll2-t500x500.jpg",
+            original_title: "Bahubali: The Beginning",
+            vote_average: 8
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000028787381-1vad7y-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000028787381-1vad7y-t500x500.jpg",
+            original_title: "Super 30",
+            vote_average: 8.5
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000108468163-dp0b6y-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000108468163-dp0b6y-t500x500.jpg",
+            original_title: "Mirzapur",
+            vote_average: 7
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000064920701-xrez5z-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000064920701-xrez5z-t500x500.jpg",
+            original_title: "John Wick",
+            vote_average: 7.5
+        }    
+    ],
+    "Comedy":
+    [
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000165384395-rhrjdn-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000165384395-rhrjdn-t500x500.jpg",
+            original_title: "3 Idiots",
+            vote_average: 9
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000185743981-tuesoj-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000185743981-tuesoj-t500x500.jpg",
+            original_title: "Iron Man",
+            vote_average: 9
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000158708482-k160g1-t500x500.jpg",
+            backdrop_path: null,
+            original_title: "Avengers Infinity War",
+            vote_average: 8.5
+        },
+        {
+            poster_path: null,
+            backdrop_path: "https://i1.sndcdn.com/artworks-000062423439-lf7ll2-t500x500.jpg",
+            original_title: "Bahubali: The Beginning",
+            vote_average: 8
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000028787381-1vad7y-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000028787381-1vad7y-t500x500.jpg",
+            original_title: "Super 30",
+            vote_average: 8.5
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000108468163-dp0b6y-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000108468163-dp0b6y-t500x500.jpg",
+            original_title: "Mirzapur",
+            vote_average: 7
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000064920701-xrez5z-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000064920701-xrez5z-t500x500.jpg",
+            original_title: "John Wick",
+            vote_average: 7.5
+        }    
+    ],
+    "Horror":
+    [
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000165384395-rhrjdn-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000165384395-rhrjdn-t500x500.jpg",
+            original_title: "3 Idiots",
+            vote_average: 9
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000185743981-tuesoj-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000185743981-tuesoj-t500x500.jpg",
+            original_title: "Iron Man",
+            vote_average: 9
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000158708482-k160g1-t500x500.jpg",
+            backdrop_path: null,
+            original_title: "Avengers Infinity War",
+            vote_average: 8.5
+        },
+        {
+            poster_path: null,
+            backdrop_path: "https://i1.sndcdn.com/artworks-000062423439-lf7ll2-t500x500.jpg",
+            original_title: "Bahubali: The Beginning",
+            vote_average: 8
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000028787381-1vad7y-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000028787381-1vad7y-t500x500.jpg",
+            original_title: "Super 30",
+            vote_average: 8.5
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000108468163-dp0b6y-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000108468163-dp0b6y-t500x500.jpg",
+            original_title: "Mirzapur",
+            vote_average: 7
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000064920701-xrez5z-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000064920701-xrez5z-t500x500.jpg",
+            original_title: "John Wick",
+            vote_average: 7.5
+        }    
+    ],
+    "Drama":
+    [
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000165384395-rhrjdn-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000165384395-rhrjdn-t500x500.jpg",
+            original_title: "3 Idiots",
+            vote_average: 9
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000185743981-tuesoj-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000185743981-tuesoj-t500x500.jpg",
+            original_title: "Iron Man",
+            vote_average: 9
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000158708482-k160g1-t500x500.jpg",
+            backdrop_path: null,
+            original_title: "Avengers Infinity War",
+            vote_average: 8.5
+        },
+        {
+            poster_path: null,
+            backdrop_path: "https://i1.sndcdn.com/artworks-000062423439-lf7ll2-t500x500.jpg",
+            original_title: "Bahubali: The Beginning",
+            vote_average: 8
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000028787381-1vad7y-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000028787381-1vad7y-t500x500.jpg",
+            original_title: "Super 30",
+            vote_average: 8.5
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000108468163-dp0b6y-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000108468163-dp0b6y-t500x500.jpg",
+            original_title: "Mirzapur",
+            vote_average: 7
+        },
+        {
+            poster_path: "https://i1.sndcdn.com/artworks-000064920701-xrez5z-t500x500.jpg",
+            backdrop_path: "https://i1.sndcdn.com/artworks-000064920701-xrez5z-t500x500.jpg",
+            original_title: "John Wick",
+            vote_average: 7.5
+        }    
+    ]
+} ;
